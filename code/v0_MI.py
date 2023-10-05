d12f178248a8f233b387b61be050c902d4646656
mgarrouste <mgarrou@umich.edu>
2023-10-05 11:37:13 -0400

    add constraints and objective

--- code/v0_MI.py
+++ code/v0_MI.py
@@ -18,13 +18,16 @@ def get_refinery_demand(refinery_id = Refinery_id_example):
   select_df = ref_df[ref_df['refinery_id']==Refinery_id_example]
   demand_sfc = float(select_df['demand_2022'])
   demand_kg_day = demand_sfc*SCF_TO_KGH2
-  return demand_kg_day
+  demand_kg_year = demand_kg_day*365
+  return demand_kg_year
 
 
 def build_model():
   model = ConcreteModel(name='Deployment at Refineries')
 
-  ## Sets ##
+
+
+  #### Sets ####
   ANR_data, H2_data = load_data()
   # SMRs
   model.G = Set(initialize=ANR_data.index)
@@ -33,15 +36,21 @@ def build_model():
   model.H = Set(initialize=list(set([elem[0] for elem in H2_data.index])))
   model.O = Set(initialize=list(range(MaxH2Mod)))
 
-  ## Variables ##
+
+
+  #### Variables ####
   model.c = Var(doc='Annualized costs')
   model.vS = Var(model.G, within=Binary, doc='Indicator of chosen ANR type')
   model.vM = Var(model.G, model.N, within=Binary, doc='Indicator of built ANR module')
   model.vP = Var(model.H, within=Binary, doc='Indicator of chosen H2 type')
   model.vQ = Var(model.H, model.O, within=Binary, doc='Indicator of built H2 module')
 
-  ## Parameters ##
-  # Demand
+
+
+  #### Parameters ####
+  # WACC 
+  model.pWACC = 0.08
+  # Demand in kg/year
   model.pRefDem = Param(initialize=get_refinery_demand)
 
   # H2 tech capacity in kg-h2/h and MWe
@@ -84,12 +93,13 @@ def build_model():
     return float(data.loc[h,'CAPEX ($/MWe)'])
   
   @model.Param(model.H)
-  def pH2LT(model, h):
+  def pH2CRF(model, h):
     data = H2_data.reset_index(level='ANR')[['Life (y)']]
     data = data.groupby(level=0).mean()
-    return float(data.loc[h,'Life (y)'])
-
-  # Capacity of ANRs MWth
+    crf = model.pWACC / (1 - (1+model.pWACC)**float(data.loc[h,'Life (y)']))
+    return crf
+  
+  # Capacity of ANRs MWt
   @model.Param(model.G)
   def pANRCap(model, g):
     return float(ANR_data.loc[g]['Power in MWe'])
@@ -106,10 +116,56 @@ def build_model():
   @model.Param(model.G)
   def pANRCAPEX(model, g):
     return float(ANR_data.loc[g]['CAPEX $/MWe'])
-
+  
+  @model.Param(model.G)
+  def pANRCRF(model, g):
+    return model.pWACC / (1 - (1+model.pWACC)**float(ANR_data.loc[g,'Life (y)']))
+    
   @model.Param(model.G)
   def pANRThEff(model, g):
     return float(ANR_data.loc[g]['Power in MWe']/ANR_data.loc[g]['Power in MWt'])
+
+
+
+  #### Objective ####
+  def annualized_cost(model):
+    return sum(model.pANRCap[g]*model.pANRThEff[g]*model.vM[g,n]*((model.pANRCAPEX[g]*model.pANRCRF[g]+model.pANRFC[g])+model.pANRVOM[g]) for n in model.N for g in model.G)\
+      + sum(model.pH2CapElec[h,g]*model.vQ[h,o]*(model.pH2CAPEX[h]*model.pH2CRF[h]+model.pH2FC[h]+model.pH2VOM[h]) for o in model.O for h in model.H for g in model.G)
+  model.Cost = Objective(rule=annualized_cost, sense=minimize)  
+
+
+  #### Constraints ####
+  # Meet refinery demand
+  def meet_ref_demand(model):
+    return model.pRefDem <= sum(model.vQ[h,o]*model.pH2CapH2[h]*365*24 for o in model.O for h in model.H)
+  model.meet_ref_demand = Constraint(rule=meet_ref_demand)
+
+  # At least one ANR module of any type deployed
+  def min_ANR_mod(model):
+    return 1 <= sum(model.vM[g,n] for n in model.N for g in model.G)
+  model.min_ANR_mod = Constraint(rule=min_ANR_mod)
+
+  # At least one H2 module of any type deployed
+  def min_H2_mod(model):
+    return 1 <= sum(model.vQ[h,o] for o in model.O for h in model.H)
+  model.min_H2_mod = Constraint(rule=min_H2_mod)
+
+  # Only one type of ANR deployed 
+  def max_ANR_type(model):
+    return 1 == sum(model.vS[g] for g in model.G)
+  model.max_ANR_type = Constraint(rule=max_ANR_type)
+
+  # Only modules of the chosen ANR type can be built
+  def match_ANR_type(model, g, n):
+    return model.vM[g,n] <= model.vS[g]
+  model.match_ANR_type = Constraint(model.G, model.N, rule=match_ANR_type)
+
+  # Heat and electricity balance
+  def heat_elec_balance(model, g):
+    return sum(model.pH2CapH2[h]*model.vQ[h,o]*(model.pH2ElecCons[h,g]/model.pANRThEff[g]) \
+      + model.pH2CapH2[h]*model.vQ[h,o]*model.pH2HeatCons[h,g] for o in model.O for h in model.H)\
+        <= sum(model.pANRCap[g]*model.vM[g,n] for n in model.N)
+  model.heat_elec_balance = Constraint(model.G, rule=heat_elec_balance)
 
   return model
 
