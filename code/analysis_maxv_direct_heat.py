ec51e645762d2f30ae93a68aca85f446342de5d5
Marisol Garrouste <mgarrou@umich.edu>
2024-04-23 10:12:13 -0400

    plotting function for comparison of noak and foak results

--- code/analysis_maxv_direct_heat.py
+++ code/analysis_maxv_direct_heat.py
@@ -6,12 +6,16 @@ import os, glob
 import utils
 
 lifetime = 34 # years
-foak = True
+foak = False
 
 cambium_scenario = 'MidCase'
 year = 2024
 
 def get_data():
+  """Loads Max Vanatta data for direct process heat generation from ANRS
+  Goes through each sheet, corresponding to each benchmark price
+  Concatenates all the data into one dataframe with existing columns and new 'NG price ($/MMBtu)' column
+  """
   heat_dfs = []
   benchmark_prices = [6,8,10,12,14]
   for price in benchmark_prices:
@@ -26,6 +30,12 @@ def get_data():
 
 
 def compute_net_annual_revenues(heat_df, anr_tag):
+  """
+  Computes the net annual revenues for a given stage of deployment from O&M and capital costs in data
+  Sorts entries by NG price and id
+  Keeps only first occurence of facility with respect to increasing NG price so that avoided emissions are not accounted 
+  for twice
+  """
   heat_df[f'ANR Cost {anr_tag} ($/y)'] = heat_df['FOPEX']+heat_df['VOPEX']+heat_df[f'ACC_Cap_{anr_tag}']
   heat_df[f'ANR Cost {anr_tag} ($/MWt/y)'] = heat_df[f'ANR Cost {anr_tag} ($/y)']/heat_df['Installed_Cap']
   heat_df[f'Net Annual Revenues {anr_tag} ($/y)'] = heat_df['Fac_Ann_Rev']-heat_df[f'ANR Cost {anr_tag} ($/y)']
@@ -69,12 +79,14 @@ def compute_cogen(df):
   
 
 
-def save_data(df, cost_tag='', cogen_tag=''):
+def save_data(df, anr_tag='', cogen_tag=''):
   df.rename(columns={'FACILITY_ID':'id', 'STATE':'state'}, inplace=True)
-  df.to_csv(f'./results/direct_heat_maxv_results_{cost_tag}_{cogen_tag}.csv', index=False)
+  df.to_csv(f'./results/direct_heat_maxv_results_{anr_tag}_{cogen_tag}.csv', index=False)
 
 
 def plot_net_annual_revenues(df, anr_tag='FOAK', cogen_tag='nocogen'):
+  save_path = f'./results/direct_heat_maxv_anr_net_ann_revenues_{anr_tag}_{cogen_tag}.png'
+  print(f'Plot net annual revenues: {save_path}')
   fig, ax = plt.subplots(figsize=(8,6))
   df[f'Net Annual Revenues {anr_tag} (M$/MWt/y)'] = df[f'Net Annual Revenues {anr_tag} ($/y)']/(df['Installed_Cap']*1e6)
   sns.boxplot(ax=ax, data=df, y='Industry', x=f'Net Annual Revenues {anr_tag} (M$/MWt/y)', hue='NG price ($/MMBtu)', fill=False, width=.5)
@@ -85,7 +97,7 @@ def plot_net_annual_revenues(df, anr_tag='FOAK', cogen_tag='nocogen'):
   ax.get_legend().set_visible(False)
   fig.legend(h, l, loc='outside right upper', title='NG price ($/MMBtu)')
   fig.tight_layout()
-  fig.savefig(f'./results/direct_heat_maxv_anr_net_ann_revenues_{anr_tag}_{cogen_tag}.png')
+  fig.savefig(save_path)
 
 
 def compute_net_annual_costs_ng_ccus(df):
@@ -94,6 +106,8 @@ def compute_net_annual_costs_ng_ccus(df):
 
 
 def plot_anr_vs_ng_ccus_costs(df, anr_tag='FOAK', cogen=False):
+  save_path = f'./results/direct_heat_maxv_cost_comparison_anr_ng_ccus_{anr_tag}_{cogen}.png'
+  print(f'Plot ANR v.s. NG with CCUS costs :{save_path}')
   # NG with CCUS cost on the x axis
   x = 'NG CCUS Cost (M$/MWt/y)'
   df[x] = df['NG CCUS Cost ($/MWt/y)']/1e6
@@ -113,10 +127,12 @@ def plot_anr_vs_ng_ccus_costs(df, anr_tag='FOAK', cogen=False):
   ax.set_xlabel(f'{x}\nCCUS cost: {utils.ccus_cost} $/t-CO2')
 
   fig.tight_layout()
-  fig.savefig(f'./results/direct_heat_maxv_cost_comparison_anr_ng_ccus_{anr_tag}_{cogen}.png')
+  fig.savefig(save_path)
 
 
 def plot_anr_vs_ng_costs(df, anr_tag='FOAK', cogen=False):
+  save_path = f'./results/direct_heat_maxv_cost_comparison_anr_ng_wo_ccus_{anr_tag}_{cogen}.png'
+  print(f'Plot ANR v.s. NG without CCUS costs : {save_path}')
   # NG with CCUS cost on the x axis
   x = 'NG wo/ CCUS Cost (M$/MWt/y)'
   df[x] = df['Fac_Ann_Rev']/(df['Thermal MWh/hr']*1e6)
@@ -136,22 +152,66 @@ def plot_anr_vs_ng_costs(df, anr_tag='FOAK', cogen=False):
   ax.set_xlabel(f'{x}')
 
   fig.tight_layout()
-  fig.savefig(f'./results/direct_heat_maxv_cost_comparison_anr_ng_wo_ccus_{anr_tag}_{cogen}.png')
+  fig.savefig(save_path)
+
+
+
+def plot_deployment_comparison_2():
+  noak_results = './results/direct_heat_maxv_results_NOAK_nocogen.csv'
+  foak_results = './results/direct_heat_maxv_results_FOAK_nocogen.csv'
+  assert os.path.isfile(noak_results), f'NOAK results not found: {noak_results}'
+  assert os.path.isfile(foak_results), f'FOAK results not found: {foak_results}'
+  noak_df = pd.read_csv(noak_results)
+  foak_df = pd.read_csv(foak_results)
+  noak_len, foak_len = len(noak_df), len(foak_df)
+  total_df = pd.merge(left=noak_df, right=foak_df, on='id', how='left')
+  # Check merging
+  assert noak_len == len(total_df)
+  # Differential net annual revenues in M$/MWt/y
+  total_df['Net Annual Revenues FOAK ($/y)'] = total_df['Net Annual Revenues FOAK ($/y)'].fillna(0)
+  total_df = total_df[~(total_df['Industry_x'] == 'Other_Not Found')]
+  total_df['Delta Net Rev'] = (total_df['Net Annual Revenues NOAK ($/y)'] - total_df['Net Annual Revenues FOAK ($/y)'])/(total_df['Installed_Cap_x']*1e6)
+  industries = total_df['Industry_x'].unique()
+  industries_labels = {'Ethyl Alcohol':'Ethyl\nAlcohol', 'Paper Mills':'Paper\nMills', 'Petroleum Refineries':'Petroleum\nRefineries', 
+                       'Alkalies and Chlorine':'Alkalies\nand\nChlorine', 'Lime':'Lime', 'Iron and Steel Mills': 'Iron and\nSteel', 
+                       'Wet Corn Milling':'Wet Corn\nMilling', 'Petro- and organic chemicals':'Petro-\nand organic\nchemicals', 
+                       'Plastics Materials and Resins':'Plastics',
+                         'Nitrogenous Fertilizers':'Nitrogenous\nFertilizers'}
+  fig, ax = plt.subplots(figsize=(10,8), sharex=True)
+  sns.boxplot(ax=ax, data=total_df, y='Industry_x', x='Delta Net Rev', hue='NG price ($/MMBtu)_x', fill=False, width=.5, palette='flare')
+  ax.set_ylabel('')
+  ax.set_xlim(-0.02, 0.085)
+  ax.set_xlabel('Different Net Annual Revenues (M$/MWt/y)')
+  ax.axvline(x=0, color='grey', linestyle='--', linewidth=1)
+  sns.despine()
+  ax.grid(True)
+  h, l = ax.get_legend_handles_labels()
+  ax.get_legend().set_visible(False)
+  fig.legend(h, l, loc='outside right upper', title='NG price ($/MMBtu)')
+  fig.tight_layout()
+  fig.savefig('./results/direct_heat_maxv_comparison_FOAK_NOAK_anr_net_ann_revenues.png')
+
+
+
 
 
 def main():
   if foak: anr_tag = 'FOAK'
   else: anr_tag = 'NOAK'
+  print(anr_tag)
   heat_df = get_data()
   heat_df = compute_net_annual_revenues(heat_df, anr_tag=anr_tag)
   heat_df = compute_net_annual_costs_ng_ccus(heat_df)
   #heat_df = compute_cogen(heat_df)
   
-  save_data(heat_df, cost_tag=anr_tag, cogen_tag='nocogen')
-  plot_net_annual_revenues(heat_df, anr_tag=anr_tag, cogen_tag='nocogen')
-  plot_anr_vs_ng_costs(heat_df, anr_tag=anr_tag)
-  plot_anr_vs_ng_ccus_costs(heat_df, anr_tag=anr_tag)
-
+  save_data(heat_df, anr_tag=anr_tag, cogen_tag='nocogen')
+ 
+  if anr_tag == 'NOAK':
+    plot_deployment_comparison_2()
+  else:
+    plot_net_annual_revenues(heat_df, anr_tag=anr_tag, cogen_tag='nocogen')
+    plot_anr_vs_ng_costs(heat_df, anr_tag=anr_tag)
+    plot_anr_vs_ng_ccus_costs(heat_df, anr_tag=anr_tag)
 
 
 if __name__ == '__main__':
