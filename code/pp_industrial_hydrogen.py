26bbf95bacce4fc8ed97ebb5b5b18aca5685a2c1
Marisol Garrouste <mgarrou@umich.edu>
2024-04-25 08:22:50 -0400

    Average cashflow breakdown plot for industries

--- code/pp_industrial_hydrogen.py
+++ code/pp_industrial_hydrogen.py
@@ -61,11 +61,43 @@ def plot_net_annual_revenues(df):
   fig.savefig(save_path, bbox_inches='tight')
 
 
+
+def plot_mean_cashflows(df):
+  save_path = f'./results/industrial_hydrogen_anr_avg_cashflows_{OAK}_cogen_{cogen_tag}.png'
+  print(f'Plot net annual revenues: {save_path}')
+  # Cashflows in M$/MWt/y
+  df['ANR CAPEX'] = -df['ANR CAPEX ($/year)']*df['Thermal Efficiency']/(1e6*df['Depl. ANR Cap. (MWe)'])
+  df['H2 CAPEX'] = -df['H2 CAPEX ($/year)']*df['Thermal Efficiency']/(1e6*df['Depl. ANR Cap. (MWe)'])
+  df['ANR O&M'] = -df['ANR O&M ($/year)']*df['Thermal Efficiency']/(1e6*df['Depl. ANR Cap. (MWe)'])
+  df['H2 O&M'] = -df['H2 O&M ($/year)']*df['Thermal Efficiency']/(1e6*df['Depl. ANR Cap. (MWe)'])
+  df['NG Avoided Costs'] = df['Avoided NG costs ($/year)']*df['Thermal Efficiency']/(1e6*df['Depl. ANR Cap. (MWe)'])
+  df['H2 PTC'] = df['H2 PTC Revenues ($/year)']*df['Thermal Efficiency']/(1e6*df['Depl. ANR Cap. (MWe)'])
+  ind_df = df[['Industry','ANR CAPEX', 'H2 CAPEX', 'ANR O&M', 'H2 O&M', 'NG Avoided Costs', 'H2 PTC']]
+  design_df = df[['ANR type','ANR CAPEX', 'H2 CAPEX', 'ANR O&M', 'H2 O&M', 'NG Avoided Costs', 'H2 PTC']]
+  ind_df = ind_df.groupby('Industry').mean()
+  print(ind_df)
+  fig, ax = plt.subplots(figsize = (8,6))
+  ind_df.plot(ax = ax, kind ='bar', stacked=True)
+  ax.set_ylabel('Average Normalized Cashflows (M$/MWt/y)')
+  ax.set_xlabel('')
+  ax.axhline(y=0, color='grey', linestyle='--', linewidth=0.5)
+  ax.set_xticks(ax.get_xticks(), ax.get_xticklabels(), rotation=0, ha='center')
+  ax.set_ylim(-0.42, 0.62)
+  ax.yaxis.set_ticks(np.arange(-0.4, 0.7, 0.1))
+  ax.get_legend().set_visible(False)
+  handles, labels = ax.get_legend_handles_labels()
+  fig.legend(handles, labels,  bbox_to_anchor=(.5,1.08),loc='upper center', ncol=3)
+  fig.tight_layout()
+  fig.savefig(save_path, bbox_inches='tight')
+
+
+
 def main():
   total_df = load_data(OAK, BE)
   total_df = compute_normalized_net_revenues(total_df, OAK)
   total_df.to_csv('./results/temp.csv')
   plot_net_annual_revenues(total_df)
+  plot_mean_cashflows(total_df)
 
 
 if __name__ == '__main__':
