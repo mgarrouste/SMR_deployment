9e8cef79bcdc088cdb8cbe1c41c85b59ac4774c2
Marisol Garrouste <mgarrou@umich.edu>
2024-05-06 07:54:38 -0400

    working script for recap plot

--- code/ANR_application_comparison.py
+++ code/ANR_application_comparison.py
@@ -10,15 +10,17 @@ elec_results_path = f'./results/price_taker_{anr_tag}_MidCase.xlsx'
 
 h2_results_path = f'./results/clean_results_anr_{anr_tag}_h2_wacc_0.077.xlsx'
 
-heat_results_path = f'./results/direct_heat_maxv_results_{anr_tag}_{cogen_tag}.csv'
+heat_results_path = f'./results/process_heat/best_pathway_{anr_tag}_{cogen_tag}.xlsx'
 
 color_map = {'Industrial Hydrogen':'blue', 'Direct Process Heat':'red', 'Total':'Green'}
 
 def load_elec_results(elec_results_path):
   elec_df = pd.read_excel(elec_results_path, index_col=0)
-  elec_df['Net Annual Revenues (M$/MWe/y)'] = elec_df['Annual Net Revenues ($/year/MWe)']/1e6
+  elec_df['Annual Net Revenues (M$/MWe/y)'] = elec_df['Annual Net Revenues ($/year/MWe)']/1e6
   # Only keep the best design for each state and year
-  df = elec_df.loc[elec_df.groupby('state')['Net Annual Revenues (M$/MWe/y)'].transform(max) == elec_df['Net Annual Revenues (M$/MWe/y)']]
+  df = elec_df.loc[elec_df.groupby('state')['Annual Net Revenues (M$/MWe/y)'].transform(max) == elec_df['Annual Net Revenues (M$/MWe/y)']]
+  df['Application'] = 'Electricity'
+  df.rename(columns={'ANR type':'ANR'}, inplace=True)
   return df
   
 
@@ -34,15 +36,17 @@ def load_h2_results(h2_results_path):
     list_df.append(df)
   all_df = pd.concat(list_df)
   all_df['Application'] = 'Industrial Hydrogen'
+  all_df['ANR'] = all_df['ANR type']
+  all_df['Annual Net Revenues (M$/MWe/y)'] = all_df['Net Revenues ($/year)']/(all_df['Depl. ANR Cap. (MWe)']*1e6)
   all_df.sort_values(by='Breakeven price ($/MMBtu)', inplace=True)
   return all_df 
 
 
 def load_heat_results(heat_results_path):
-  heat_df = pd.read_csv(heat_results_path, index_col='id')
-  # Sort by NG benchmark price and revenues = avoided NG cost -> Increaw
-  heat_df[f'Net Annual Revenues {anr_tag} (M$/MWt/y)'] # TODO
-  heat_df.sort_values(by=['NG price ($/MMBtu)', f'Net Annual Revenues {anr_tag} ($/y)'], inplace=True)
+  """Loads direct process heat results and returns them sorted by breakeven prices"""
+  heat_df = pd.read_excel(heat_results_path, index_col='FACILITY_ID')
+  heat_df['Annual Net Revenues (M$/MWe/y)']  = heat_df['Pathway Net Ann. Rev. (M$/y)']/heat_df['Depl. ANR Cap. (MWe)']
+  heat_df.sort_values(by=['Breakeven NG price ($/MMBtu)', 'Annual Net Revenues (M$/MWe/y)'], inplace=True)
   heat_df['Application'] = 'Direct Process Heat'
   return heat_df
 
@@ -94,7 +98,7 @@ def plot_cumulative_avoided_emissions(applications_results):
     ax[1].stairs(values, edges, label=app, color=color_map[app], baseline=None)
   ax[1].set_xlim(-2,xmax)
   ax[1].xaxis.set_ticks(np.arange(0, xmax+10, 10))
-  ax[1].set_xlabel('NG price ($/MMBtu)')
+  ax[1].set_xlabel('Breakeven NG price ($/MMBtu)')
   ax[1].set_ylabel('')
   ax[1].grid(True)
   ax[1].spines['top'].set_visible(False)
@@ -104,22 +108,29 @@ def plot_cumulative_avoided_emissions(applications_results):
   lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
   lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
   fig.legend(lines, labels, loc='upper right', ncol=1)
-  fig.savefig('./results/viable_avoided_emissions.png')
+  fig.savefig(f'./results/all_applications_viable_avoided_emissions_{anr_tag}_{cogen_tag}.png')
 
 
 
-def plot_net_annual_revenues_all_app(applications_results):
-  fig, ax = plt.subplots(len(applications_results),1, sharex=True, figsize=(10,8))
+
+def concat_results(results):
+  df_list = []
+  for app, app_data in results.items():
+    temp_df = app_data['data']
+    temp_df['Application'] = app 
+    df_list.append(temp_df)
+  df = pd.concat(df_list, ignore_index=True)
+  return df
+
+
+def plot_net_annual_revenues_all_app(df):
+  fig, ax = plt.subplots(figsize=(10,8))
   count = 0
-  palette={'HTGR':'blue', 'iMSR':'orange', 'iPWR':'green', 'PBR-HTGR':'darkorchid', 'Microreactor':'darkgrey'}
-  for app, app_data in applications_results.items():
-    sns.stripplot(ax=ax[count], data=app_data['data'], x='Annual Net Revenues (M$/MWe/y)', y=app,\
-                  palette=palette, hue='Reactor', marker='*', size=7, alpha=0.8)
-    if count !=0:
-      ax[count].get_legend().set_visible(False)
-    ax[count].set_ylabel('')
-    ax[count].grid(True)
-    count +=1
+  palette={'HTGR':'blue', 'iMSR':'orange', 'iPWR':'green', 'PBR-HTGR':'darkorchid', 'Micro':'darkgrey'}
+  sns.stripplot(ax=ax, data=df, x='Annual Net Revenues (M$/MWe/y)', y='Application',\
+                  palette=palette, hue='ANR', size=7, alpha=0.8)
+  sns.boxplot(ax=ax, data=df, x='Annual Net Revenues (M$/MWe/y)', y='Application', color='black',\
+                  fill=False, width=0.5)
   fig.tight_layout()
   plt.show()
   #fig.savefig('./results/ANR_application_comparison.png')
@@ -128,19 +139,20 @@ def main():
   h2_df = load_h2_results(h2_results_path=h2_results_path)
   h2_df = compute_cumulative_avoided_emissions(h2_df)
   heat_df = load_heat_results(heat_results_path=heat_results_path)
-  heat_df = compute_cumulative_avoided_emissions(heat_df, emissions_label='Emissions')
+  heat_df = compute_cumulative_avoided_emissions(heat_df, emissions_label='Emissions_mmtco2/y')
   applications_results = {'Industrial Hydrogen':{'data':h2_df, 
                                                  'emissions_label':'Ann. avoided CO2 emissions (MMT-CO2/year)',
                                                  'price_label':'Breakeven price ($/MMBtu)'}
-                          , 'Direct Process Heat':{'data':heat_df, 
-                                                   'emissions_label':'Emissions',
-                                                   'price_label':'NG price ($/MMBtu)'}}
+                          ,'Direct Process Heat':{'data':heat_df, 
+                                                   'emissions_label':'Emissions_mmtco2/y',
+                                                   'price_label':'Breakeven NG price ($/MMBtu)'}}
   plot_cumulative_avoided_emissions(applications_results)
   elec_df = load_elec_results(elec_results_path)
   applications_results['Electricity'] = {'data':elec_df, 
                                          'emissions_label':None, 
                                          'price_label':None}
-  plot_net_annual_revenues_all_app(applications_results)
+  results = concat_results(applications_results)
+  plot_net_annual_revenues_all_app(results)
 
 if __name__ == '__main__':
   main()
