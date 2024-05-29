a4aac778684fbe7ad471f9486f11c9c8d79edfa7
Marisol Garrouste <mgarrou@umich.edu>
2024-05-29 17:37:02 -0400

    size based on deployed capacity, start

--- code/map_foak.py
+++ code/map_foak.py
@@ -32,24 +32,43 @@ fig.add_trace(go.Choropleth(
 		colorscale='Reds',
 ))
 
-h2_data = ANR_application_comparison.load_h2_results(anr_tag='FOAK', cogen_tag='nocogen')
+h2_data = ANR_application_comparison.load_h2_results(anr_tag='FOAK', cogen_tag='cogen')
 h2_data = h2_data[['latitude', 'longitude', 'Depl. ANR Cap. (MWe)', 'Breakeven price ($/MMBtu)', 'Ann. avoided CO2 emissions (MMT-CO2/year)', 
-									 'Industry', 'Application', 'ANR', 'Annual Net Revenues (M$/MWe/y)' ]]
+									 'Industry', 'Application', 'ANR', 'Annual Net Revenues (M$/y)' ]]
 h2_data['Emissions_mmtco2/y'] = h2_data['Ann. avoided CO2 emissions (MMT-CO2/year)']
 h2_data['App'] = h2_data.apply(lambda x: x['Application']+'-'+x['Industry'].capitalize(), axis=1)
 h2_data.reset_index(inplace=True)
 
-heat_data = ANR_application_comparison.load_heat_results(anr_tag='FOAK', cogen_tag='nocogen')
+heat_data = ANR_application_comparison.load_heat_results(anr_tag='FOAK', cogen_tag='cogen')
 heat_data = heat_data[['latitude', 'longitude', 'Emissions_mmtco2/y', 'ANR',
 											 'Depl. ANR Cap. (MWe)', 'Industry', 'Breakeven NG price ($/MMBtu)',
-											 'Annual Net Revenues (M$/MWe/y)', 'Application']]
+											 'Annual Net Revenues (M$/y)', 'Application']]
 heat_data['App'] = 'Process Heat'
 heat_data.reset_index(inplace=True)
 
 foak_positive = pd.concat([h2_data, heat_data], ignore_index=True)
-foak_positive = foak_positive[foak_positive['Annual Net Revenues (M$/MWe/y)'] >=0]
+foak_positive = foak_positive[foak_positive['Annual Net Revenues (M$/y)'] >=0]
+print(foak_positive['Annual Net Revenues (M$/y)'].describe(percentiles=[.1,.25,.5,.75,.9]))
 
+# Size based on capacity deployed
+percentiles =  foak_positive['Depl. ANR Cap. (MWe)'].describe(percentiles=[.1,.25,.5,.75,.9]).to_frame()
+print(percentiles.loc['10%'])
+print(percentiles)
+def set_size(revenues):
+	if revenues <= percentiles.loc['10%'].iloc[0]:
+		size = 5
+	elif revenues <= percentiles.loc['25%'].iloc[0]:
+		size = 7
+	elif revenues <= percentiles.loc['50%'].iloc[0]:
+		size = 10
+	elif revenues <= percentiles.loc['75%'].iloc[0]:
+		size = 17
+	else:
+		size = 30
+	return size
 
+foak_positive['size'] = foak_positive['Depl. ANR Cap. (MWe)'].apply(set_size)
+print(foak_positive['size'])
 def plot_waterfall(foak_positive):
 	df = foak_positive[['App', 'Emissions_mmtco2/y', 'Depl. ANR Cap. (MWe)']]
 	df = df.rename(columns={'Emissions_mmtco2/y':'Emissions', 'Depl. ANR Cap. (MWe)':'Capacity'})
@@ -58,7 +77,7 @@ def plot_waterfall(foak_positive):
 	df = df.reset_index()
 	total_emissions = df['Emissions'].sum()
 	total_capacity = df['Capacity'].sum()
-	total_row = pd.DataFrame({'App': ['FOAK'], 'Emissions': [total_emissions], 'Capacity':[total_capacity]})
+	total_row = pd.DataFrame({'App': ['FOAK-Total'], 'Emissions': [total_emissions], 'Capacity':[total_capacity]})
 	df = pd.concat([df, total_row], ignore_index=True)
 
 	fig = make_subplots(rows=1, cols=2, horizontal_spacing=0.25)
@@ -110,15 +129,15 @@ def plot_waterfall(foak_positive):
 		height=550,  # Set the height of the figure
 	)
 
-	fig.write_image('./results/foak_positive_emissions_capacity.png')
+	fig.write_image('./results/foak_cogen_positive_emissions_capacity.png')
 
 plot_waterfall(foak_positive)
 
-scaler = 40
+scaler = 0.02
 
 		
 # Set marker symbol based on the application's type
-markers_applications = {'Process Heat':'square', 'Industrial Hydrogen':'circle'}
+markers_applications = {'Process Heat':'cross', 'Industrial Hydrogen':'circle'}
 marker_symbols = foak_positive['Application'].map(markers_applications).to_list()
 
 # Get colors for each marker
@@ -130,11 +149,11 @@ fig.add_trace(go.Scattergeo(
 		text="Capacity: " + foak_positive['Depl. ANR Cap. (MWe)'].astype(str) + " MWe",
 		mode='markers',
 		marker=dict(
-				size=foak_positive['Annual Net Revenues (M$/MWe/y)']*scaler+5,
-				color=foak_positive['Annual Net Revenues (M$/MWe/y)'],
+				size=foak_positive['size'],
+				color=foak_positive['Annual Net Revenues (M$/y)'],
 				colorscale='Greys',
 				colorbar = dict(
-						title='Annual Net Revenues (M$/MWe/y)',
+						title='Annual Net Revenues (M$/y)',
 						orientation='h',  # Set the orientation to 'h' for horizontal
 						x=0.5,  # Center the colorbar horizontally
 						y=-0.1,  # Position the colorbar below the x-axis
@@ -142,8 +161,8 @@ fig.add_trace(go.Scattergeo(
 						yanchor='bottom',
 						lenmode='fraction',  # Use 'fraction' to specify length in terms of fraction of the plot area
 						len=0.8,  # Length of the colorbar (80% of figure width)
-						tickvals = [0.1,0.25,0.5,0.75,1,1.25],
-						ticktext = [0.1,0.25,0.5,0.75,1,1.25],
+						tickvals = [1,50,100],
+						ticktext = [1,50,100],
 						tickmode='array'
 				),
 				symbol=marker_symbols,
@@ -212,5 +231,5 @@ fig.update_layout(
 )
 
 # Save
-fig.write_image('./results/map_FOAK.png')
+fig.write_image('./results/map_FOAK_cogen.png')
 
