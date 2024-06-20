d35a2f9b6b2e168bd85d36cf1fc4651d668e3af7
Marisol Garrouste <mgarrou@umich.edu>
2024-06-20 08:05:00 -0400

    optional elec, add irr to results

--- code/map_noPTC.py
+++ code/map_noPTC.py
@@ -2,84 +2,85 @@ import pandas as pd
 import plotly.graph_objects as go
 from utils import palette
 import ANR_application_comparison
+import os
 from utils import compute_average_electricity_prices
 
-
+with_elec = True
 # Create figure
 fig = go.Figure()
 
 
-"""
-# Electricity: average state prices and breakeven prices
-elec_path = './results/average_electricity_prices_MidCase_2024.xlsx'
-if os.path.isfile(elec_path):
-	elec_df = pd.read_excel(elec_path)
-else:
-	compute_average_electricity_prices(cambium_scenario='MidCase', year=2024)
-	elec_df = pd.read_excel(elec_path)
+if with_elec:
+	# Electricity: average state prices and breakeven prices
+	elec_path = './results/average_electricity_prices_MidCase_2024.xlsx'
+	if os.path.isfile(elec_path):
+		elec_df = pd.read_excel(elec_path)
+	else:
+		compute_average_electricity_prices(cambium_scenario='MidCase', year=2024)
+		elec_df = pd.read_excel(elec_path)
 
-# Define tick values and corresponding custom tick texts
-colorbar_ticks = [20, 30, 40, 46.1, 52.2, 56.9, 77.8, 116.9]
-colorbar_texts = ['20', '30', '40', 
-									'BE iMSR: 46', 'BE PBR-HTGR: 52', 'BE iPWR: 57', 'BE HTGR: 78', 'BE Micro: 117']
+	# Define tick values and corresponding custom tick texts
+	colorbar_ticks = [20, 30, 40, 46.1, 52.2, 56.9, 77.8, 116.9]
+	colorbar_texts = ['20', '30', '40', 
+										'BE iMSR: 46', 'BE PBR-HTGR: 52', 'BE iPWR: 57', 'BE HTGR: 78', 'BE Micro: 117']
 
-max_actual_value = max(elec_df['average price ($/MWhe)'])
-print('Maximum state-level average electricity price: ', max_actual_value)
-max_tick_value = max(colorbar_ticks)
-# List of colors for the colorscale (light to dark blue)
-color_list = ["#ebf3fb", "#d2e3f3", "#b6d2ee", "#85bcdb", "#57a0ce", "#3082be", "#1361a9", "#0a4a90", "#08306b"]
+	max_actual_value = max(elec_df['average price ($/MWhe)'])
+	print(elec_df['average price ($/MWhe)'].describe())
+	max_tick_value = max(colorbar_ticks)
+	# List of colors for the colorscale (light to dark blue)
+	color_list = ["#ebf3fb", "#d2e3f3", "#b6d2ee", "#85bcdb", "#57a0ce", "#3082be", "#1361a9", "#0a4a90", "#08306b"]
 
-# Compute the proportion of the actual max value against the maximum tick value
-actual_data_proportion = max_actual_value / max_tick_value
+	# Compute the proportion of the actual max value against the maximum tick value
+	actual_data_proportion = max_actual_value / max_tick_value
 
-# Build a normalized colorscale
-colorscale = []
-for i, color in enumerate(color_list):
-		# Normalize the color positions based on the actual data proportion and evenly distribute them
-		colorscale.append((i * actual_data_proportion / (len(color_list) - 1), color))
-# Ensure the last color anchors at 1.0
-colorscale.append((1, color_list[-1]))
+	# Build a normalized colorscale
+	colorscale = []
+	for i, color in enumerate(color_list):
+			# Normalize the color positions based on the actual data proportion and evenly distribute them
+			colorscale.append((i * actual_data_proportion / (len(color_list) - 1), color))
+	# Ensure the last color anchors at 1.0
+	colorscale.append((1, color_list[-1]))
 
-fig.add_trace(
-		go.Choropleth(
-				locationmode='USA-states',  # Set the location mode to 'USA-states'
-				locations=elec_df['state'],  # Use the 'state' column from the dataframe for locations
-				z=elec_df['average price ($/MWhe)'],  # Use the 'average_price' column from the dataframe for coloring
-				marker_line_color='white',  # Set the state boundary color
-				marker_line_width=0.5,  # Set the state boundary width
-				zmin=min(colorbar_ticks),  # Optional: Adjust the zmin if desired
-				zmax=max_tick_value,       # Extending zmax to cover custom tick values
-				autocolorscale=False,
-				colorscale=colorscale,     # Custom colorscale defined above
-				colorbar = dict(
-						title='Electricity price ($/MWhe)',
-						orientation='h',  # Set the orientation to 'h' for horizontal
-						x=0.5,  # Center the colorbar horizontally
-						y=-0.3,  # Position the colorbar below the x-axis
-						xanchor='center',
-						yanchor='bottom',
-						lenmode='fraction',  # Use 'fraction' to specify length in terms of fraction of the plot area
-						len=0.7,  # Length of the colorbar (80% of figure width)
-						tickvals=colorbar_ticks,  # Custom tick values
-						ticktext=colorbar_texts,
-						tickfont=dict(size=14)
-				),
-		)
-)
-"""
+	fig.add_trace(
+			go.Choropleth(
+					locationmode='USA-states',  # Set the location mode to 'USA-states'
+					locations=elec_df['state'],  # Use the 'state' column from the dataframe for locations
+					z=elec_df['average price ($/MWhe)'],  # Use the 'average_price' column from the dataframe for coloring
+					marker_line_color='white',  # Set the state boundary color
+					marker_line_width=0.5,  # Set the state boundary width
+					zmin=min(colorbar_ticks),  # Optional: Adjust the zmin if desired
+					zmax=max_tick_value,       # Extending zmax to cover custom tick values
+					autocolorscale=False,
+					colorscale=colorscale,     # Custom colorscale defined above
+					colorbar = dict(
+							title='Electricity price ($/MWhe)',
+							orientation='h',  # Set the orientation to 'h' for horizontal
+							x=0.5,  # Center the colorbar horizontally
+							y=-0.3,  # Position the colorbar below the x-axis
+							xanchor='center',
+							yanchor='bottom',
+							lenmode='fraction',  # Use 'fraction' to specify length in terms of fraction of the plot area
+							len=0.7,  # Length of the colorbar (80% of figure width)
+							tickvals=colorbar_ticks,  # Custom tick values
+							ticktext=colorbar_texts,
+							tickfont=dict(size=14)
+					),
+			)
+	)
+
 
 
 # FOAK data with no PTC
 h2_data = ANR_application_comparison.load_h2_results(anr_tag='FOAK', cogen_tag='cogen')
 h2_data = h2_data[['state','latitude', 'longitude', 'State price ($/MMBtu)','Depl. ANR Cap. (MWe)', 'BE wo PTC ($/MMBtu)', 'Ann. avoided CO2 emissions (MMT-CO2/year)', 
-									 'Industry', 'Application', 'ANR' ]]
+									 'Industry', 'Application', 'ANR', 'IRR wo PTC' ]]
 h2_data['application'] = h2_data.apply(lambda x:'H2-'+x['Industry'].capitalize(), axis=1)
 h2_data.rename(columns={'ANR':'SMR'}, inplace=True)
 h2_data.reset_index(inplace=True)
 
 heat_data = ANR_application_comparison.load_heat_results(anr_tag='FOAK', cogen_tag='cogen')
 heat_data = heat_data[['STATE','latitude', 'longitude', 'NG price ($/MMBtu)', 'Emissions_mmtco2/y', 'SMR',
-											 'Depl. ANR Cap. (MWe)', 'Industry','BE wo PTC ($/MMBtu)', 'Application']]
+											 'Depl. ANR Cap. (MWe)', 'Industry','BE wo PTC ($/MMBtu)', 'Application', 'IRR wo PTC']]
 heat_data.rename(columns={'Emissions_mmtco2/y':'Ann. avoided CO2 emissions (MMT-CO2/year)',
 													'NG price ($/MMBtu)':'State price ($/MMBtu)', 'STATE':'state'}, inplace=True)
 heat_data['application'] = 'Process Heat'
@@ -99,9 +100,9 @@ def plot_be_vs_state_price(data):
 	g.savefig('./results/foak_noPTC_be_vs_state.png')
 
 
-tosave_noptc = noptc_be[['id','state', 'State price ($/MMBtu)', 'BE wo PTC ($/MMBtu)', 'application', 'SMR']]
-print('Minimum state price : {}'.format(min(tosave_noptc['State price ($/MMBtu)'])))
-tosave_noptc = tosave_noptc.rename(columns={'BE wo PTC ($/MMBtu)':'Breakeven price ($/MMBtu)'})
+tosave_noptc = noptc_be[['id','state', 'application', 'SMR','State price ($/MMBtu)', 'BE wo PTC ($/MMBtu)', 'IRR wo PTC']]
+tosave_noptc = tosave_noptc.rename(columns={'BE wo PTC ($/MMBtu)':'Breakeven price ($/MMBtu)', 'IRR wo PTC':'IRR'})
+tosave_noptc['IRR'] *=100
 plot_be_vs_state_price(tosave_noptc)
 tosave_noptc.set_index('id', inplace=True)
 tosave_noptc.to_latex('./results/foak_noPTC.tex',float_format="{:0.1f}".format, longtable=True, escape=True,\
